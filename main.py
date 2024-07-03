b20288a049cc8bf9b0ccbb4dd9bb733e7283d6a5
Dr. Confuused <128633214+Untitled-Master@users.noreply.github.com>
2024-07-03 22:52:12 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 import requests
 from bs4 import BeautifulSoup
 
 app = Flask(__name__)
+CORS(app) 
 
 @app.route('/search', methods=['POST'])
 def search():
